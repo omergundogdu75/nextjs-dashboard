e0ba5d751a64a2a1dd0c1c55dab5cc2ffbefcca6
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2024-02-06 00:21:50 +0300

    12.BÖLÜM use server ile devam edecek

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   function handleSearch(term: string) {
     console.log(term);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
     if (term) {
       params.set('query', term);
     } else {
@@ -31,6 +33,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
+
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
